a3a4efaf5e2f489b28c083d58602672858c3bcd2
heavye <euelfantaye@gmail.com>
2022-03-07 05:13:40 +0000

    sum

--- heavy/models/data_source/source_mod.sql
+++ heavy/models/data_source/source_mod.sql
@@ -3,9 +3,20 @@
 with source_sales as (
 
         
-    SELECT AVG("Deal_Value") AS avg_value_perweek FROM {{ source("adludio", "sales_table") }}
+    SELECT 
+        DATE_PART('week', CAST("Deal_created_at" AS DATE)) as week,
+       
+        COUNT(DATE_PART('week', CAST("Deal_created_at" AS DATE))) as weekcount,
+
+        SUM("Deal_Value") AS sum_value_per_week, 
+        SUM("Deal_Email_messages_count") AS sum_emails_per_week  
     
-               GROUP BY DATE_PART('week', CAST("Deal_created_at" AS DATE))
+    FROM {{ source("adludio", "sales_table") }}
+    
+    GROUP BY week
+             
+
+    ORDER BY week
     ),
 
 final as (
